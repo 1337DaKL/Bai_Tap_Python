880018de2c092972f6f47a76a9e138387632994e
Trịnh Đắc Lượng <luongtrinh2k3ndad@gmail.com>
2024-09-20 15:49:25 +0700

    Delete site1 directory

--- site1/app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    # Add other URL patterns here
-    path('/1', views.index1, name='index1'),
-    path('/out', views.getout, name='getout')
-]
